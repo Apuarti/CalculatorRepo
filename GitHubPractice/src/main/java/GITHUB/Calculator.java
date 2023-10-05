6a74a79d7c7e0346498b1327e4c5420355c64944
Apuarti <you@example.com>
2023-10-04 21:51:05 -0500

    committing through eclipse

---
+++ GitHubPractice/src/main/java/GITHUB/Calculator.java
@@ -0,0 +1,16 @@
+package GITHUB;
+
+public class Calculator {
+	
+	
+	public int multiply(int a, int b)
+	{
+		return a*b;
+		
+	}
+	public static void main(String[] args) {
+		Calculator cal= new Calculator();
+		System.out.println(cal.multiply(3, 10));
+	}
+
+}
